9347d176e21e54989bca7741c94a4289e699d52f
rob verhoef <16617@ma-web.nl>
2015-06-08 11:51:22 +0200

    Started work on PlayerControls, Added more assets

--- Assets/Scripts/CameraPixelPerfect.cs
+++ Assets/Scripts/CameraPixelPerfect.cs
@@ -6,6 +6,6 @@ public class CameraPixelPerfect : MonoBehaviour
 	
 	void Awake () 
 	{
-		GetComponent<Camera>().orthographicSize = ((float)Screen.height) / 2;
+		GetComponent<Camera>().orthographicSize = ((float)Screen.height) /1;
 	}
 }
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour 
+{
+ 	private float _speed = 500;
+	private float _jumpSpeed = 100;
+	private Rigidbody _rigidBody;
+
+	void Awake ()
+	{
+		_rigidBody = this.GetComponent<Rigidbody>();
+	}
+
+	void Update () 
+	{
+		float walkTranslation = Input.GetAxis ("Horizontal") * _speed;
+		walkTranslation *= Time.deltaTime;
+		transform.Translate(walkTranslation, 0, 0);
+
+
+	}
+}
