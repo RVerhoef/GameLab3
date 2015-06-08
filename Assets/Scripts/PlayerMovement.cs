dd7a675b346dfc92ea973786c9df72248be5717a
rob verhoef <16617@ma-web.nl>
2015-06-08 15:40:44 +0200

    Worked on movement, Added animations, Added assets

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,12 +4,15 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
  	private float _speed = 500;
-	private float _jumpSpeed = 100;
+	private float _jumpSpeed = 50000;
+	public float _direction;
 	private Rigidbody _rigidBody;
+	private Animator _animator;
 
 	void Awake ()
 	{
 		_rigidBody = this.GetComponent<Rigidbody>();
+		_animator = this.GetComponent <Animator>();
 	}
 
 	void Update () 
@@ -19,5 +22,17 @@ public class PlayerMovement : MonoBehaviour
 		transform.Translate(walkTranslation, 0, 0);
 
 
+
+		if(Input.GetButton ("Jump") && _animator.GetBool("Jumping") == false)
+		{ 
+			Debug.Log("Jump!");
+			_animator.SetBool("Jumping",true);
+			_rigidBody.AddForce(transform.up * _jumpSpeed);
+		}
+	}
+
+	void OnCollisionEnter (Collision other)
+	{
+		_animator.SetBool("Jumping",false);
 	}
 }
