790598f03a3ffc535eecc4df9fe6aceb9b59f97a
rob verhoef <16617@ma-web.nl>
2015-06-08 16:25:34 +0200

    Refinded movement, Added turning, Added animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour 
 {
  	private float _speed = 500;
-	private float _jumpSpeed = 50000;
-	public float _direction;
+	private float _jumpSpeed = 30000;
+	private float _direction = 1;
 	private Rigidbody _rigidBody;
 	private Animator _animator;
 
@@ -17,21 +17,36 @@ public class PlayerMovement : MonoBehaviour
 
 	void Update () 
 	{
-		float walkTranslation = Input.GetAxis ("Horizontal") * _speed;
-		walkTranslation *= Time.deltaTime;
-		transform.Translate(walkTranslation, 0, 0);
+		this.transform.Translate ((Input.GetAxis ("Horizontal") * (_speed)) * Time.deltaTime, 0, 0);
 
+		if(Input.GetAxis ("Horizontal") > 0)
+		{
+			_direction = 1;
+		}
+		else if(Input.GetAxis ("Horizontal") < 0)
+		{
+			_direction = -1;
+		}
 
+		transform.localScale = new Vector3(_direction ,1 ,1);
+
+		if(Input.GetButton ("Horizontal") && _animator.GetBool("Jumping") == false)
+		{
+			_animator.SetBool("Walking",true);
+		}
+		else
+		{
+			_animator.SetBool("Walking",false);
+		}
 
 		if(Input.GetButton ("Jump") && _animator.GetBool("Jumping") == false)
 		{ 
-			Debug.Log("Jump!");
 			_animator.SetBool("Jumping",true);
 			_rigidBody.AddForce(transform.up * _jumpSpeed);
 		}
 	}
 
-	void OnCollisionEnter (Collision other)
+	void OnCollisionEnter (Collision collision)
 	{
 		_animator.SetBool("Jumping",false);
 	}
