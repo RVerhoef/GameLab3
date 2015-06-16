beaafdbaa509bf12221a81ee0ae2c6cf644f4249
Collix <15986@ma-web.nl>
2015-06-16 12:23:14 +0200

    Merge remote-tracking branch 'origin/master'
    Bird

---
+++ Assets/Scripts/BirdShitBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdShitBehaviour : MonoBehaviour
+{
+    public float speed = 5;
+    public float deleteHeight = 100;
+    
+    private Vector3 fallDir;
+    
+    
+    // Use this for initialization
+    void Start()
+    {
+        fallDir = new Vector3((1 * 1) * speed, -0.5f * speed * 5, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position += fallDir;
+
+        if(this.transform.position.y < deleteHeight)
+        {
+            Destroy(this.gameObject, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/PigeonController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PigeonController : MonoBehaviour
+{
+    public float speed = 5;
+    public float cruiseHeight = 625;
+    public float shitCooldown = 0.1f;
+    public GameObject birdShit;
+    public ParticleSystem hitPS;
+
+    private bool stationary = true;
+    private Vector3 liftDir;
+    private Vector3 cruiseDir;
+    private float cTime; 
+
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        liftDir = new Vector3(0, 1 * speed, 1 * this.transform.localScale.x);
+        cruiseDir = new Vector3((1 * this.transform.localScale.x) * speed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(stationary == false)
+        {
+            if(this.transform.position.y < cruiseHeight)
+            {
+                this.transform.position += liftDir;
+            }
+            else
+            {
+                this.transform.position += cruiseDir;
+                cTime += Time.deltaTime;
+                if(cTime >= shitCooldown)
+                {
+                    shit();
+                    cTime = 0;
+                }
+            }
+        }
+
+    }
+    
+    void shit()
+    {
+        Instantiate(birdShit, this.transform.position, this.gameObject.transform.rotation);
+    }
+    
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("yolo");
+        if(other.gameObject.tag == "Player")
+        {
+            hitPS.Play();
+            stationary = false;
+        }
+    }
+}
